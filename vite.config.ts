a3c0803d36c814857df8f86eb4332f2a6c909d32
KrisEnigma <krisescobarm@gmail.com>
2026-05-27 20:48:37 -0400

    Fix local editor API responses

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,13 @@ export default defineConfig({
   }],
   server: {
     host: true,
-    port: 5173
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'https://ludodex.krisenigma.com',
+        changeOrigin: true,
+        secure: true
+      }
+    }
   }
 });
